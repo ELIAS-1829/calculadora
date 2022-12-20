04e17e83d73b79d6e8b3e4f8af28eb09de0a4a21
ELIAS-1829 <121119836+ELIAS-1829@users.noreply.github.com>
2022-12-20 14:12:38 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#creador elias-1829
+while :
+do
+
+clear
+echo -e "\033[38;1;33m"
+echo "
+[*]creadores elias.1829 y solitario²⁰²²
+[*]herramienta =calcularora
+
+
+"
+echo -e "\033[38;1;36m" "::::::::::::::: HACKER-TOOL :::::::::::::::"
+      echo -e "\033[38;1;31m" ""
+echo -e "\033[38;5;33m" " :::: colaboracion con legionimous reina :::: "
+echo -e "\033[38;1;36m"
+echo "
+_1_ suma
+_2_ resta 
+_3_ multiplicacion
+_4_ Divicion 
+_5_ Raiz cuadrada 
+_0_ Salir
+"
+OPC=6 
+echo -e "\033[38;1;34m"
+read  -p "Eliga una opcion " OPC
+echo ""
+
+
+
+if	[ $OPC -gt 7 ];then
+echo -e "\033[38;1;31m"
+       echo " :::: ERROR DIGITE DE NUEVO :::'"
+       echo ''
+       sleep 1.2
+
+      
+elif   [ $OPC -eq 1 ];then
+       echo -e "\033[38;1;33m"
+       read -p "digite un numero " n1
+       read -p "digite otro numero " n2 
+echo "" 
+       echo -e "\033[38;0;32m" "EL RESULTADO ES $[ $n1 + $n2 ]"
+
+sleep 1.6
+bash elias.sh 
+elif   [ $OPC -eq 2 ];then
+       echo -e "\033[38;1;33m"
+       read -p "digite un numero " n1
+       read -p "digite otro numero " n2
+       echo -e "\033[38;0;32m" "EL RESULTADO ES $[ $n1 - $n2 ]"
+sleep 1.6
+bash elias.sh
+elif   [ $OPC -eq 3 ];then
+       echo -e "\033[38;1;33m"
+       read -p "digite un numero " n1
+       read -p "digite otro numero " n2
+       echo -e "\033[38;0;32m" "EL RESULTADO ES $[ $n1 * $n2 ]"
+sleep 1.6
+bash elias.sh
+elif   [ $OPC -eq 4 ];then
+       echo -e "\033[38;1;33m" 
+       read -p "digite un numero " n1
+       read -p "digite otro numero " n2
+       echo -e "\033[38;0;32m" "EL RESULTADO ES $[ $n1 / $n2 ]"
+sleep 1.6
+bash elias.sh
+elif   [ $OPC -eq 5 ];then
+       echo -e "\033[38;1;33m"
+       read -p "digite un numero " n1
+       read -p "digite otro numero " n2
+       echo -e "\033[38;0;32m" "EL RESULTADO ES $[ $n1 | $n2 ]"
+elif   [ $OPC -eq 0 ];then
+ echo -e "\033[38;1;31m" "LG-REINA MANDA "
+fi
+done 
